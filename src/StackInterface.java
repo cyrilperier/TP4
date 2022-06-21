fff93a06ce850a26f381e065a59ec1dc952f0a39
Carla <carla.faboumy@epfedu.fr>
2022-06-21 13:48:43 -0400

    STACK

--- src/StackInterface.java
+++ src/StackInterface.java
@@ -1,7 +1,12 @@
-public interface StackInterface {
-    public void push(Object t);
-    public Object pop();
-    public Object top();
-    public boolean isEmpty();
-    public int size();
+/****************
+ Monitoring d'appels de méthodes: exemple de la pile
+ Cours: 8INF958
+ Auteur: Klaus Havelund
+****************/
+interface StackInterface {
+	public void push (Object t);
+	public Object pop();
+	public Object top();
+	public boolean isEmpty();
+	public int size();	
 }
